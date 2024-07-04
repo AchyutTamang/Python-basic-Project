e4ac4c05e4ba707ee33f6127dec0f429847b8430
AchyutTamang <achyuttamang23@gmail.com>
2024-07-04 23:48:20 +0545

    If else

---
+++ main.py
@@ -0,0 +1,33 @@
+# Basic If else operation
+# Total_amount = 5000
+# charge = 10 
+# withdraw = int(input("Input enter the amount u want to withdraw: "))
+
+# if withdraw % 500 == 0:
+#     if withdraw >= Total_amount + charge:
+#         print("Insufficent balance")
+#     else:
+#         print("Transication is ssucessfull ")
+#         print("YOur current balance is ", Total_amount - withdraw - charge)
+# else:
+#     print("Enter the divisible of 500")
+
+
+# num1 = int(input("ENter the first number :"))
+# num2 = int(input("Enter the second number :"))
+
+# operater = input("Enter operation : ")
+
+# if operater == "+":
+#     print(num1+num2)
+
+# elif operater == "-":
+#     print(num1-num2)
+
+# elif operater == "/":
+#     print(num1/num2)
+
+# elif operater == "*":
+#     print(num1*num2)
+# else:
+#     print("enter a valid operator")
