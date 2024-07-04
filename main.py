0d47b2cbac47efcf875c8305edb59366897c64bd
AchyutTamang <achyuttamang23@gmail.com>
2024-07-04 23:53:38 +0545

    if else

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 # Basic If else operation
+
+
+"""
+Suppose u would like to withdraw X amount of cash from an ATM. 
+The ATM will only accept the transaction if x is a multiple of 500,
+and ur account balance has enough cash to perform the withdrawal transaction (including bank charges). 
+For each successful withdrawal the bank charges NPR 10. Calaculate poojas account balance after an attempted transaction.
+
+"""
 # Total_amount = 5000
 # charge = 10 
 # withdraw = int(input("Input enter the amount u want to withdraw: "))
@@ -12,7 +21,10 @@
 # else:
 #     print("Enter the divisible of 500")
 
-
+"""
+Write a Python program that takes two numbers and an operator (+, -, *, /) as input from the user
+and performs the corresponding arithmetic operation.
+"""
 # num1 = int(input("ENter the first number :"))
 # num2 = int(input("Enter the second number :"))
 
